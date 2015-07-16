ef87ebd58804e0caae30210bd923d8cd009cf747
Sébastien Veluz <sebastien.veluz@gmail.com>
2015-07-16 11:54:55 +0200

    Add json file data and functionnalities for loading/querying/displaying data with lokijs

--- pocs/browser-database/lokijs/js/app.js
+++ pocs/browser-database/lokijs/js/app.js
@@ -7,11 +7,41 @@ app.lokijs= (function(self){
     var budgets;
     
     self.init = function(){
+      self.btnLoadDatabase = document.getElementById('load-database');
+      self.btnDisplayData = document.getElementById('display-data');
+      self.btnClearData = document.getElementById('clear-data');
       db = new loki('lokidb');
       budgets = db.addCollection('budgets');
       
-      loadRandomDocuments(nb);
-      displayItemsList();
+      app.lokijs.Events.init();
+    };
+    
+    self.loadJsonFiles = function(){
+      console.time("Reading json data");
+      $.getJSON("data/objects100.json", function(data) {
+        budgets.insert(data);
+        console.log('Row inserted Successfully');
+        console.timeEnd("Reading json data");
+        console.timeEnd("Importing data on the database");
+      });
+    };
+    
+    self.displayItemsList = function(){
+      budgets.data.forEach(function(item) {
+        var node = document.createElement("li");
+        var accountInfo = item.year;
+        item.accounts.forEach(function(account){
+          accountInfo += " " + account.name +" "+ account.value;
+        });
+        
+        node.appendChild(document.createTextNode(accountInfo));
+        document.getElementById('items').appendChild(node);
+      });
+      console.timeEnd("Reading and display data");
+    };
+    
+    self.clearData = function(){
+     $('#items').empty();
     };
     
     function loadRandomDocuments(nb){
@@ -32,24 +62,37 @@ app.lokijs= (function(self){
       }
     }
     
-    function displayItemsList(){
-      budgets.data.forEach(function(item) {
-        var node = document.createElement("li");
-        var accountInfo = item.year;
-        item.accounts.forEach(function(account){
-          accountInfo += " " + account.name +" "+ account.value;
-        });
-        
-        node.appendChild(document.createTextNode(accountInfo));
-        document.getElementById('items').appendChild(node);
-      });
-    }
-    
     function getRandomInt(min, max){
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     
     return self;
 })(app.lokijs || {});
+
+app.lokijs.Events = (function(self){
+    
+    self.init = function(){
+        app.lokijs.btnLoadDatabase.onclick = clkLoadDatabase;
+        app.lokijs.btnDisplayData.onclick = clkDisplayData;
+        app.lokijs.btnClearData.onclick =  clkClearData;
+    };
+
+    function clkLoadDatabase(){
+      console.time("Importing data on the database");
+      app.lokijs.loadJsonFiles();
+    }
+    
+    function clkDisplayData(){
+      console.time("Reading and display data");
+      app.lokijs.displayItemsList();
+    }
+    
+    function clkClearData(){
+      app.lokijs.clearData();
+    }
+
+    return self;
+})(app.lokijs.Events || {});
+
 
 app.lokijs.init();
